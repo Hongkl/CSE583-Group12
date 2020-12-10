7ddfafedf5ded7c0cb81592305238c1fdfda7c36
jingcao <jc338@uw.edu>
2020-12-10 01:29:12 -0800

    12.10

--- covid_wa/tests/test_covid.py
+++ covid_wa/tests/test_covid.py
@@ -4,7 +4,7 @@ UnitTests(unittest.TestCase) -- this class is a unit test which includes 5 diffe
 
 """
 import unittest
-from codebase.covid_wa import TooBox
+from covid_wa.covid_web import TooBox
 
 class UnitTests(unittest.TestCase):
     """"This class is a unit test which includes 8 different test for ToolBox.py."""
