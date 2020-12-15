fae996b91f5de49cd3835fc0209144cbe9e35abd
Hongkl <Hongkuanlin@gmail.com>
2020-12-14 22:18:42 -0800

    Edited COVIDWA

--- COVIDWA/CovidWeb.py
+++ COVIDWA/CovidWeb.py
@@ -2,9 +2,9 @@
 they could run this module to interact and browse the Covid and unemployment data on the map.
 The map will be displayed on a web page automatically. The user therefore should have a browser.
 This module includes a class called CovidWeb which is inherited from "param.Parameterized".
-We mainly use "panel" library to realize the widgets and the dashboard.Panel supports using 
-parameters and dependencies between parameters as expressed by "param" in a simple way to 
-encapsulate dashboards as declarative, self-contained classes.
+We mainly use "panel" library to realize the widgets and the dashboard.
+Panel supports using parameters and dependencies between parameters as expressed.
+by "param" in a simple way toencapsulate dashboards as declarative, self-contained classes.
 Users don't need to input anything on the web page, just choose the period and the county name,
 all the information or data will be loaded automatically.
 """
@@ -14,7 +14,6 @@ import folium
 import json
 import param
 import os
-import webbrowser
 import ToolBox
 
 pn.extension()
@@ -42,37 +41,37 @@ class CovidWeb(param.Parameterized):
 
     @param.depends('Counties','Months')
     def unemploy_text(self):
-        county=self.Counties
-        month=self.Months
+        county = self.Counties
+        month = self.Months
         return ToolBox.unemploy_text_parser(county,month)
 
     def getGeoAndData(self):
-        state_geo=json.load(open("../data/WA_County_Boundaries.geojson"))
-        state_data=pd.read_csv("../data/COVID19/COVID19-Rate.csv")
+        state_geo = json.load(open("../data/WA_County_Boundaries.geojson"))
+        state_data = pd.read_csv("../data/COVID19/COVID19-Rate.csv")
         return state_geo, state_data
 
     @param.depends('Counties','Months')                                         
     def popup(self):
         Map=folium.Map(location=[47.351076, -120.640135], zoom_start=6.5)
         #popup Unemployment data
-        searchCounty='../data/WA_County_json/'+ self.Counties + '.json'
+        searchCounty = '../data/WA_County_json/'+ self.Counties + '.json'
         Text=self.unemploy_text()
         layer1=folium.features.GeoJson(data=json.load(open(searchCounty)), tooltip = folium.map.Tooltip(text = Text)).add_to(Map)
 
         #popup Covid data             
-        state_geo,state_data=self.getGeoAndData()
+        state_geo,state_data = self.getGeoAndData()
         month = self.Months
         layer2 = folium.Choropleth(
-            geo_data=state_geo,
-            name='choropleth',
-            data=state_data,
-            columns=['County', month],
-            key_on='feature.properties.JURISDICT_NM',
-            fill_color='OrRd',
-            fill_opacity=0.7,
-            line_opacity=0.2,
-            threshold_scale=[0, 0.1, 0.2, 0.4, 0.6, 0.8, 1.2, 1.8],
-            legend_name='Covid19 Infection Rate (%)',
+            geo_data = state_geo,
+            name = 'choropleth',
+            data = state_data,
+            columns = ['County', month],
+            key_on = 'feature.properties.JURISDICT_NM',
+            fill_color = 'OrRd',
+            fill_opacity = 0.7,
+            line_opacity = 0.2,
+            threshold_scale = [0, 0.1, 0.2, 0.4, 0.6, 0.8, 1.2, 1.8],
+            legend_name = 'Covid19 Infection Rate (%)',
         ).add_to(Map)
         Map.keep_in_front(layer2, layer1)
         folium.LayerControl().add_to(Map)
--- COVIDWA/ToolBox.py
+++ COVIDWA/ToolBox.py
@@ -1,40 +1,41 @@
 import pandas as pd
-months={'March', 'April', 'May', 'June', 'July', 'August', 'September'}
-counties={"Adams", "Asotin", "Benton", "Chelan", "Clallam", "Clark", "Columbia", 
-            "Cowlitz", "Douglas", "Ferry", "Franklin", "Garfield", "Grant", "Grays Harbor", 
-            "Island", "Jefferson", "King", "Kitsap", "Kittitas", "Klickitat", "Lewis", 
-            "Lincoln", "Mason", "Okanogan", "Pacific", "Pend Oreille", "Pierce", "SanJuan", 
-            "Skagit", "Skamania", "Snohomish", "Spokane", "Stevens", "Thurston", "Wahkiakum", 
+months = {'March', 'April', 'May', 'June', 'July', 'August', 'September'}
+counties = {"Adams", "Asotin", "Benton", "Chelan", "Clallam", "Clark", "Columbia", 
+            "Cowlitz", "Douglas", "Ferry", "Franklin", "Garfield", "Grant", "Grays Harbor",
+            "Island", "Jefferson", "King", "Kitsap", "Kittitas", "Klickitat", "Lewis",
+            "Lincoln", "Mason", "Okanogan", "Pacific", "Pend Oreille", "Pierce", "SanJuan",
+            "Skagit", "Skamania", "Snohomish", "Spokane", "Stevens", "Thurston", "Wahkiakum",
             "Walla Walla", "Whatcom", "Whitman", "Yakima"}
-
-def unemploy_text_parser(county,month):
+def unemploy_text_parser(county, month):
     if month not in months:
         raise ValueError("Wrong month value input")
+
     if county not in county:
         raise ValueError("Wrong county value input")
-    data=pd.read_csv("../data/Unemployed/Unemployment.csv")
-    countyName=county + " " + "County"
-    searchCounty=data[data['County Name/State Abbr']==countyName]
+
+    data = pd.read_csv("../data/Unemployed/Unemployment.csv")
+    countyName = county + " " + "County"
+    searchCounty = data[data['County Name/State Abbr'] == countyName]
     # Here should insert an exceptioncatch, if we don't have a dic
-    searchFinal=searchCounty[searchCounty['Period']==month]
+    searchFinal = searchCounty[searchCounty['Period'] == month]
     # Here should insert an exceptioncatch
-    text="County Name: " + searchFinal['County Name/State Abbr'].item()+'     '\
-    "Period: " + searchFinal['Period'].item()+ '     ' \
+    text = "County Name: " + searchFinal['County Name/State Abbr'].item()+'     '
+    "Period: " + searchFinal['Period'].item()+ '     '
     "Unemployment Rate: " + str(searchFinal['Rate'].item())          
     return text
 
 def plot(county):
-    county=county + " " + "County"
-    covid_data=pd.read_csv("../data/COVID19/COVID19-Rate and Unemployment.csv")
-    df_covid=pd.DataFrame(covid_data.loc[covid_data['County']==county, 
+    county = county + " " + "County"
+    covid_data = pd.read_csv("../data/COVID19/COVID19-Rate and Unemployment.csv")
+    df_covid = pd.DataFrame(covid_data.loc[covid_data['County'] == county, 
                                          ['March', 'April', 'May', 'June', 'July', 'August', 'September']].T)
-    df_unemployment=pd.DataFrame(covid_data.loc[covid_data['County']==county, ['March_umemployment', 'April_umemployment',                         'May_umemployment', 'June_umemployment', 'July_umemployment', 'August_umemployment', 'Septembe_umemployment']].T) 
-    df_unemployment.rename({'March_umemployment': 'March'}, axis='index',inplace=True)
-    df_unemployment.rename({'April_umemployment': 'April'}, axis='index',inplace=True)
-    df_unemployment.rename({'May_umemployment': 'May'}, axis='index',inplace=True)
-    df_unemployment.rename({'June_umemployment': 'June'}, axis='index',inplace=True)
-    df_unemployment.rename({'July_umemployment':'July'}, axis='index',inplace=True)
-    df_unemployment.rename({'August_umemployment': 'August'}, axis='index',inplace=True)
-    df_unemployment.rename({'Septembe_umemployment':'September'}, axis='index',inplace=True)
-    df_covid.plot(title=county + ' infection rate', xlabel='Month', ylabel='Rate %')
-    df_unemployment.plot(title=county + ' unemployment rate', xlabel='Month', ylabel='Rate %')
+    df_unemployment = pd.DataFrame(covid_data.loc[covid_data['County']==county, ['March_umemployment', 'April_umemployment',                        'May_umemployment', 'June_umemployment', 'July_umemployment', 'August_umemployment', 'Septembe_umemployment']].T) 
+    df_unemployment.rename({'March_umemployment':'March'}, axis='index', inplace=True)
+    df_unemployment.rename({'April_umemployment':'April'}, axis='index', inplace=True)
+    df_unemployment.rename({'May_umemployment':'May'}, axis='index', inplace=True)
+    df_unemployment.rename({'June_umemployment':'June'}, axis='index', inplace=True)
+    df_unemployment.rename({'July_umemployment':'July'}, axis='index', inplace=True)
+    df_unemployment.rename({'August_umemployment': 'August'}, axis='index', inplace=True)
+    df_unemployment.rename({'Septembe_umemployment':'September'}, axis='index', inplace=True)
+    df_covid.plot(title = county + ' infection rate', xlabel='Month', ylabel='Rate %')
+    df_unemployment.plot(title = county + ' unemployment rate', xlabel='Month', ylabel='Rate %')
