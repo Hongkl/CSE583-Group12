6d7791b6a100e74828796e47a35e90348696ebc2
Hongkl <Hongkuanlin@gmail.com>
2020-12-15 17:04:12 -0800

    Edit tests

--- tests/test_covid.py
+++ tests/test_covid.py
@@ -4,6 +4,7 @@ UnitTests(unittest.TestCase) -- this class is a unit test which includes 5 diffe
 
 """
 import unittest
+import pandas as pd
 from COVIDWA import ToolBox
 
 data = pd.read_csv("Unemployment.csv")
